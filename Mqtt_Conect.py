e460f4af05818c0f05d9c2f649fd2bd9e06ceef5
Utzura <169121570+Utzura@users.noreply.github.com>
2025-11-13 11:46:58 -0500

    Update Mqtt_Conect.py

--- Mqtt_Conect.py
+++ Mqtt_Conect.py
@@ -1,12 +1,6 @@
 import json
 import paho.mqtt.client as mqtt
 
-    client1= paho.Client("z4m")                           
-    client1.on_publish = on_publish                          
-    client1.connect(broker,port)  
-    message =json.dumps({"Act1":choice})
-    ret= client1.publish("cmqtt_z4m", message)
-
 broker="broker.mqttdashboard.com"
 port=1883
 client1= paho.Client("z4m")
